2348792287dac580bebfdb1ea194aa46ebab6fca
kodeinnovate <kodeinnovate@gmail.com>
2023-10-30 12:05:49 +0530

    new file:   assets/images/kodeinnovate.png new file:   assets/images/login_image.png new file:   lib/app_colors.dart modified:   lib/main.dart modified:   pubspec.yaml

---
+++ lib/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color customBackground = Color(0xFF1138F7);
+  static const Color textColor = Colors.black;
+  static const Color primaryColor = Colors.blue;
+  // Add more color constants as needed
+}
