658fa927ab231ee234999a7ade96ec8d6a46df3b
kodeinnovate <kodeinnovate@gmail.com>
2023-12-07 17:08:29 +0530

    modified:   lib/Controllers/data_fetch_controller.dart

--- lib/Controllers/data_fetch_controller.dart
+++ lib/Controllers/data_fetch_controller.dart
@@ -1,7 +1,6 @@
 import 'package:get/get.dart';
 import 'package:kode_rx/Controllers/user_repo.dart';
 import 'package:kode_rx/database/medicine_data_fetch.dart';
-import 'package:kode_rx/select_medicenes.dart';
 
 class DataController extends GetxController {
   static DataController get instance => Get.find();
@@ -10,17 +9,4 @@ class DataController extends GetxController {
   Future<List<MedicineModel>> getAllMedicine() async {
     return await _userRepo.getMedicineList();
   }
-
-  // Future<List<Medicine>> getAllMedicine() async {
-  //   List<MedicineModel> medicineModels = await _userRepo.getMedicineList();
-
-  //   // Convert MedicineModel to Medicine
-  //   List<Medicine> medicines = medicineModels.map((model) {
-  //     return Medicine(model.id!, model.medicineName);
-  //   }).toList();
-
-  //   return medicines;
-  // }
-
-  
 }
